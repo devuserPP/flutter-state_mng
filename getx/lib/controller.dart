66588eca21981b0ba0af9c0f27483768ad4886ed
devuserPP <devuser@example.com>
2021-05-24 16:42:07 +0200

    added some comments

--- getx/lib/controller.dart
+++ getx/lib/controller.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+//make variable "count" observable just by add  ".obs"
+// every time you change variable "count", all widgets that use it are automatically changed.
 class Controller extends GetxController {
   var count = 0.obs;
   increment() => count++;
